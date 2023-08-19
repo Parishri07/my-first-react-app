c4641a513f88db1dde7e8ccef9477cf45142e261
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-19 16:13:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="/">TextUtils</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/">About</a>
-      </li>
-    </ul>
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+    <Navbar title="TextUtils" aboutText="About TextUtlis"/>
+    {/* <Navbar title="TextUtils2"/> */}
+    {/* <Navbar /> */}
     </>
   );
 }
 //you can return only one tag
 //when you have to use JS element or string then use parenthesis({})
-//replace class with className
+//replace  with use className instead of class 
+//here title and aboutText are props/properties
 
 export default App;
