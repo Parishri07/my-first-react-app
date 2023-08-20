efc537e6615343f9ba5211bbb8fe9f96a1f519f9
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-08-20 16:07:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
+
 
 function App() {
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About TextUtlis"/>
     {/* <Navbar title="TextUtils2"/> */}
     {/* <Navbar /> */}
+    <Navbar title="TextUtils" aboutText="About TextUtlis"/>
+    <TextForm heading="Enter text to analyse"/>
     </>
   );
 }
